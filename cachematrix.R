b09e8942dd66221cbbc34b9f7bc83ceee735fc32
reudismam <reudismam@gmail.com>
2014-08-19 19:32:11 -0300

    removing unnecessary comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,3 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
 ##Cache the inverted matrix. This function if 
 ##called used in cacheSolve function
 makeCacheMatrix <- function(x = matrix()) {
@@ -12,23 +8,18 @@ makeCacheMatrix <- function(x = matrix()) {
                 s <<- NULL
         }
         ##return the matrix
-        get <- function() x
-        
+        get <- function() x        
         ##Set the inverted matrix in s
         setsolve <- function(solve) s <<- solve
-        
         ##Get the inverted matrix s
         getsolve <- function() s
-        
         ##List representation of cached inverted matrix
         list(set = set, get = get,
              setsolve = setsolve,
              getsolve = getsolve)
-
 }
 
 
-## Write a short comment describing this function
 ##This function only calculate the matrix 
 ##invertion if the was not calculate yet.
 cacheSolve <- function(x, ...) {
@@ -37,8 +28,7 @@ cacheSolve <- function(x, ...) {
         if(!is.null(s)) {
                 message("getting cached data")
                 return(s)
-        }
-        
+        }        
         #Get the matrix that is stored in makeCacheMatrix x
         data <- x$get()
         ##Calculate the inversion matrix
